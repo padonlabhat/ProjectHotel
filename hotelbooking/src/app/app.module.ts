7e1cb8d9d2a2e10272bf1d77b947e52aeec8cc09
BADSTARSS <73570438+BADSTARSS@users.noreply.github.com>
2021-10-31 14:17:17 +0700

    create component on hotelbooking
    สร้างcomponent เพิมมานะครับเท่าที่จำได้

--- hotelbooking/src/app/app.module.ts
+++ hotelbooking/src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SimplePageComponent } from './simple-page/simple-page.component';
+import { SearchComponent } from './search/search.component';
+import { LoginComponent } from './login/login.component';
+import { CancelbookingComponent } from './cancelbooking/cancelbooking.component';
+import { BookingComponent } from './booking/booking.component';
+import { PaymentComponent } from './payment/payment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SimplePageComponent
+    SimplePageComponent,
+    SearchComponent,
+    LoginComponent,
+    CancelbookingComponent,
+    BookingComponent,
+    PaymentComponent
   ],
   imports: [
     BrowserModule,
