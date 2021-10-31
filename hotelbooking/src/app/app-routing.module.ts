d6a9830b1b97748f458d9fd939f43fd9d5141647
nataponntp <originalntp25430828@gmail.com>
2021-10-31 16:36:31 +0700

    Create Bar and router-path

--- hotelbooking/src/app/app-routing.module.ts
+++ hotelbooking/src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookingComponent } from './booking/booking.component';
+import { CancelbookingComponent } from './cancelbooking/cancelbooking.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+import { PaymentComponent } from './payment/payment.component';
+import { SearchComponent } from './search/search.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent},
+  { path: 'cancelbooking', component: CancelbookingComponent},
+  { path: 'payment', component: PaymentComponent},
+  { path: 'booking', component: BookingComponent},
+  { path: 'search', component: SearchComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [
+  PaymentComponent,
+  LoginComponent,
+  CancelbookingComponent,
+  BookingComponent,
+  SearchComponent
+]
--- hotelbooking/src/app/app.module.ts
+++ hotelbooking/src/app/app.module.ts
@@ -1,24 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SimplePageComponent } from './simple-page/simple-page.component';
-import { SearchComponent } from './search/search.component';
-import { LoginComponent } from './login/login.component';
-import { CancelbookingComponent } from './cancelbooking/cancelbooking.component';
-import { BookingComponent } from './booking/booking.component';
-import { PaymentComponent } from './payment/payment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SimplePageComponent,
-    SearchComponent,
-    LoginComponent,
-    CancelbookingComponent,
-    BookingComponent,
-    PaymentComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
