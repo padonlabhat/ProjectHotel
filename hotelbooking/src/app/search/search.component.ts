dc7556cb91cc223e00f6850cd651a29015692c55
BADSTARSS <73570438+BADSTARSS@users.noreply.github.com>
2021-11-06 11:00:54 +0700

    update search

--- hotelbooking/src/app/search/search.component.ts
+++ hotelbooking/src/app/search/search.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
 
-
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
@@ -9,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
   constructor() { }
- 
+ term : any;
   list = [{ 
       id: 1, name: 'Room1' , country: 'India', price : '2000' },
     { id: 2, name: 'Room2' , country: 'USA', price : '1500'},
