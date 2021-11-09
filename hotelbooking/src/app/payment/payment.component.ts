fbcb9feacc900671f1c345c3b3f6d26705bd71bd
gamezzz121 <gamezzz121@gmail.com>
2021-11-09 23:59:21 +0700

    add table database on payment page
    database from firebase payment
    not have input data

--- hotelbooking/src/app/payment/payment.component.ts
+++ hotelbooking/src/app/payment/payment.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { AngularFireDatabase,AngularFireList }  from 'angularfire2/database';
 
-import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+
+import { Observable, ReplaySubject } from 'rxjs';
+
 
 
 
@@ -16,13 +19,50 @@ export class PaymentComponent implements OnInit {
   number_card= '';
   expiry_card= '';
   cvv_card= '';
-  public hotelbooking: Observable<any>[] = [];
-  constructor(private db: AngularFireDatabase) { 
-    this.db.object('payment/0').set({id : 1,name :  'game'})
+  public _name!: string;
+  public _number!: number;
+  public _expiry!: number;
+  public _cvv!: number;
 
-}
+  public _paymentList!: Payment[];
+
+
+
+  constructor(private db: AngularFireDatabase) { 
+ this.db.object('payment/0').set({name : 'game',number : '033215454554668',expiry :  '0425',cvv : '425'})
+  this.getStarted();
+ }
+ async getStarted(){
+    var payment: Payment[];
+    await this. getPaymentsFromRealtimeDB().then(value => {
+      payment = value as Payment[];
+     this._paymentList = payment;
+    });
+    
+    console.log(this._paymentList)
+    //this._order = this._paymentList[this._paymentList.length-1].id +1
+
+ }
+ getPaymentsFromRealtimeDB(){
+   return new Promise((resolve, reject)=>{
+    this.db.list('payment').valueChanges().subscribe(value =>{
+      resolve(value);
+    })
+   });
+ }
+ 
   ngOnInit(): void {
     
   }
+  
+
+
+}
+
+class Payment {
+  name!: string;
+  number!: number;
+  expiry!: number;
+  cvv!: number;
 
 }
