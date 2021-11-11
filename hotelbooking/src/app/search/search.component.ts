3b12cad0431eab6778a48e2b9295f94ff6cd87a1
BADSTARSS <73570438+BADSTARSS@users.noreply.github.com>
2021-11-11 22:21:39 +0700

    edit search

--- hotelbooking/src/app/search/search.component.ts
+++ hotelbooking/src/app/search/search.component.ts
@@ -2,18 +2,55 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Observable } from 'rxjs';
 import {AngularFireDatabase} from 'angularfire2/database';
-
+import {MessageService} from 'primeng/api';
+import { PrimeNGConfig } from 'primeng/api';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
+  providers: [MessageService]
 })
 export class SearchComponent implements OnInit {
-  constructor(private db: AngularFireDatabase) { 
+
+
+  public roomN!: string;
+  public roomID!: string;
+  public price!: number;
+  public description!:String;
+ 
+
+  public search!: search[];
+
+  constructor(private db: AngularFireDatabase,private messageService: MessageService, private primengConfig: PrimeNGConfig) { 
+
+this.getStarted();
+this.addBtn();
 
 
 
 }
+addBtn(){}
+
+save(){}
+async getStarted(){
+  var search: search[];
+  await this. getroomDB().then(value => {
+    search = value as search[];
+   this.search = search;
+  });
+  
+  console.log(this.search)
+ // this._order = this.search[this.search.length-1].order +1
+  //console.log(this._order)
+}
+getPaymentsFromRealtimeDB(){
+ return new Promise((resolve, reject)=>{
+  this.db.list('payment').valueChanges().subscribe(value =>{
+    resolve(value);
+  })
+ });
+}
+
 
  term : any;
   list = [{ 
@@ -38,17 +75,50 @@ export class SearchComponent implements OnInit {
     { id: 19, name: 'Room19' , country: 'South Africa', price : '4500'},
     { id: 20, name: 'Room20' , country: 'Sri Lanka', price : '3000'}
   ];
+
+
   
   ngOnInit(): void {
+    this.primengConfig.ripple = true;
   }
  
 addWiki (data : NgForm){
   console.log(data.value);
-  this.db.list("/hotelbooking").push(data.value);
+  this.db.list("/roombooking").push(data.value);
 }
 
-
+getroomDB(){
+  return new Promise((resolve, reject)=>{
+   this.db.list('roombooking').valueChanges().subscribe(value =>{
+     resolve(value);
+   })
+  });
 }
 
+showSuccess() {
+  this.messageService.add({key: 'tc',severity:'success', summary: 'Success', detail: 'Message Content'});
+}
+showError() {
+this.messageService.add({severity:'error', summary: 'Error', detail: 'Message Content'});
+}
+onConfirm() {
+this.messageService.clear('c');
+}
+
+onReject() {
+this.messageService.clear('c');
+}
+
+clear() {
+this.messageService.clear();
+}
+
+}
+class search{
+  roomN! :string;
+  roomID! :string;
+  price!:number;
+  description!:string;
+}
 
 
