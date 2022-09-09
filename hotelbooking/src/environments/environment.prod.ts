1a47c6e4ffe1b64461734236a017592f4f56fa95
Padonlabhat Sitthiparn <padonlabhatsit@gmail.com>
2022-09-10 01:56:40 +0700

    Add firebase to Production

--- hotelbooking/src/environments/environment.prod.ts
+++ hotelbooking/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD3B8CAwS8Nu0i17FyEEE__obn43Cp_NFo",
+    authDomain: "hotelbooking-832a2.firebaseapp.com",
+    databaseURL: "https://hotelbooking-832a2-default-rtdb.firebaseio.com",
+    projectId: "hotelbooking-832a2",
+    storageBucket: "hotelbooking-832a2.appspot.com",
+    messagingSenderId: "611899950851",
+    appId: "1:611899950851:web:7bbaa44dc25078a22dcb36",
+    measurementId: "G-5FX9X24KZ0"
+  }
 };
